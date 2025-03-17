4783eb3e6193434217d6623fc1d93c28ed11efe1
CyberNomadX <kyleduffy89@gmail.com>
2025-03-17 11:36:34 -0700

    added subfinder and amass find and merge script v0.1

---
+++ osint_subdomains.sh
@@ -0,0 +1,23 @@
+#Set domain from arguement.
+domain=$1
+
+if [ -z "$domain" ]; then
+    echo "[*] Usage: $0 <domain>"
+    exit 1
+fi
+
+#Output file for results
+OutputFile="subdomains_$domain.txt"
+
+echo "[*] Running Subfinder on $domain"
+subfinder -d $domain -o subfinder_results.txt
+
+echo "[*] Running Amass on $domain (passive mode)"
+echo "Patience...(this may take a while)"
+amass enum -passive -d $domain -o amass_results.txt
+
+echo "[*] Merging, organizing and sorting results"
+echo "[*] Removing duplicates"
+cat subfinder_results.txt amass_results.txt | sort -u > $OutputFile
+
+echo "[*] Enumeration complete. Results saved to $OutputFile"
