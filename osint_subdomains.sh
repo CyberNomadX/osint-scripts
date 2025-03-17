28074e859e2ff0df0143d97b0e434ca9dc4f6d1d
CyberNomadX <kyleduffy89@gmail.com>
2025-03-17 14:48:54 -0700

    Color coded error messages added with tput

--- osint_subdomains.sh
+++ osint_subdomains.sh
@@ -1,9 +1,9 @@
 
 #Define color variables
-GREEN="\e[32m"
-YELLOW="\e[33m"
-RED="\e[31m"
-RESET="\e[0m"
+RED=$(tput setaf 1)
+GREEN=$(tput setaf 2)
+YELLOW=$(tput setaf 3)
+NC=$(tput sgr0)
 
 #Set domain from arguement.
 domain=$1
@@ -11,21 +11,21 @@ domain=$1
 #TODO: Add checks for tools required
 #Check if subfinder is installed
 if command -v subfinder >&2; then
-    echo "${GREEN} ✅ Subfinder is installed"
+    echo "${GREEN} ✅ Subfinder is installed.${NC}"
 else
-    echo "${RED}❌ Subfinder is not installed"
-    echo "Please install subfinder and try again"
-    read -p "Press [Enter] to exit the process"
+    echo "${RED}❌ Subfinder is not installed.${NC}"
+    echo "${YELLOW}Please install subfinder and try again.${NC}"
+    read -p "${YELLOW}Press [Enter] to exit the process.${NC}"
     exit 1
 fi
 
 #Check if amass is installed
 if command -v amass >&2; then
-    echo "${GREEN} ✅ Amass is installed"
+    echo "${GREEN} ✅ Amass is installed${NC}"
 else
-    echo "${RED}❌ Amass is not installed"
-    echo "Please install amass and try again"
-    read -p "Press [Enter] to exit the process"
+    echo "${RED}❌ Amass is not installed${NC}"
+    echo "${YELLOW}Please install amass and try again"
+    read -p "${YELLOW}Press [Enter] to exit the process"
     exit 1
 fi
 
@@ -41,11 +41,11 @@ echo "[*] Running Subfinder on $domain"
 subfinder -d $domain -o subfinder_results.txt
 
 echo "[*] Running Amass on $domain (passive mode)"
-echo "Patience...(this may take a while)"
+echo "⏳ Patience...(this may take a while)"
 amass enum -passive -d $domain -o amass_results.txt
 
 echo "[*] Merging, organizing and sorting results"
 echo "[*] Removing duplicates"
 cat subfinder_results.txt amass_results.txt | sort -u > $OutputFile
 
-echo "[*] Enumeration complete. Results saved to $OutputFile"
+echo "${GREEN}[*] Enumeration complete. Results saved to $OutputFile ${NC}"
